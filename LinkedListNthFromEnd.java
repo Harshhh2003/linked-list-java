840ef11d68575f5f1fcb80af7d7f6417938cc2dd
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-11-17 16:30:08 +0530

    Fix nth node from end in linked list

---
+++ LinkedListNthFromEnd.java
@@ -0,0 +1,63 @@
+
+
+public class LinkedListNthFromEnd {  
+    node head;
+
+    
+    public static node nthnodelast(node head, int n) {
+        int size = 0;
+        node temp = head;
+
+        
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+
+        
+        if (n > size) {
+            System.out.println("The list has fewer than " + n + " node");
+            return null;
+        }
+
+        int m = size - n + 1;
+        temp = head;
+
+        for (int i = 1; i < m; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
+  
+    public static class node {
+        int data; 
+        node next;
+
+        node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        node y = new node(150);
+        node z = new node(80);
+        node a = new node(60);
+        node b = new node(90);
+        node h = new node(20);
+        
+
+        y.next = z;
+        z.next = a;
+        a.next = b;
+        b.next = h;
+
+
+        node result = nthnodelast(y, 4);
+        if (result != null) {
+            System.out.println("The 4th node from the end is: " + result.data);
+        }
+    }
+}
