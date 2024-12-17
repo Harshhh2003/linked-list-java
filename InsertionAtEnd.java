4d2e9071c90e2181591bb13360943db69916d93b
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-12-17 23:05:45 +0530

    Implemented insertion at the start of the doubly linked list

---
+++ InsertionAtEnd.java
@@ -0,0 +1,57 @@
+public class InsertionAtEnd {
+
+    public static node insertAtEnd(node head, int x) {
+        node t = new node(x);
+        if (head == null) {
+            head = t;
+        } else {
+            node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = t;
+            t.prev = temp;
+        }
+        return head;
+    }
+
+    public static void display(node head) {
+        node temp = head;
+        while (temp != null) {  System.out.print(temp.data + "   ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static class node {
+        int data;
+        node next, prev;
+
+        node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        node a = new node(1000);
+        node b = new node(101);
+        node c = new node(230);
+        node d = new node(110);
+
+        a.prev = null;
+        a.next = b;
+        b.prev = a;
+        b.next = c;
+        c.prev = b;
+        c.next = d;
+        d.prev = c;
+        d.next = null;
+
+        System.out.println("Original linked list:");
+        display(a);
+
+        a = insertAtEnd(a, 500); // Insert 500 at the end
+        System.out.println("Linked list after insertion at the end:");
+        display(a);
+    }
+}
